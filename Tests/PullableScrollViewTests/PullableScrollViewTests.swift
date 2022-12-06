3cb8c62cd6096537b834a2bd65f6e50e83173745
Pantelis Giazitsis <giazitsispantelis@gmail.com>
2022-12-06 11:36:12 +0200

    Minor fixes

--- Tests/PullableScrollViewTests/PullableScrollViewTests.swift
+++ Tests/PullableScrollViewTests/PullableScrollViewTests.swift
@@ -5,14 +5,16 @@ import SwiftUI
 final class PullableScrollViewTests: XCTestCase {
 
     func testUsage() {
-        XCTAssertNoThrow(PullableScrollView(tintColor: .red,
-                                            refreshCallback: { callback in
-            // Fetch some data and invoke `callBack` to hide the indicator
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                callback()
-            }
-        }, content: {
-            Text("Scroll view content")
-        }))
+        XCTAssertNoThrow(
+            PullableScrollView(tintColor: .red,
+                               refreshCallback: { callback in
+                                   // Fetch some data and invoke `callBack` to hide the indicator
+                                   DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                                       callback()
+                                   }
+                               }, content: {
+                                   Text("Scroll view content")
+                               })
+        )
     }
 }
