da9d37dc041c2554a7ea163f6bdc5b5d3027cc99
Pantelis Giazitsis <giazitsispantelis@gmail.com>
2023-01-20 18:56:40 +0200

    Hiding scroll indicators for iOS 16

--- Sources/PullableScrollView/PullableScrollView.swift
+++ Sources/PullableScrollView/PullableScrollView.swift
@@ -14,31 +14,38 @@ import SwiftUI
 /// }
 /// ```
 public struct PullableScrollView<Content: View>: View {
-
+    
     public typealias VoidCallback = () -> Void
     public typealias RefreshCallback = (@escaping VoidCallback) -> Void
-
+    
     private let tintColor: Color?
     private let showsVerticalScrollIndicator: Bool
     private let content: Content
     private let refreshCallback: RefreshCallback
-
+    
     public init(tintColor: Color?, showsVerticalScrollIndicator: Bool = false, refreshCallback: @escaping RefreshCallback, @ViewBuilder content: () -> Content) {
         self.tintColor = tintColor
         self.showsVerticalScrollIndicator = showsVerticalScrollIndicator
         self.content = content()
         self.refreshCallback = refreshCallback
     }
-
+    
     public var body: some View {
         List {
             content
-            .listRowBackground(Color.clear)
-            .listRowSeparator(.hidden)
-            .listRowInsets(EdgeInsets())
+                .listRowBackground(Color.clear)
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets())
         }
         .listStyle(.plain)
         .listSectionSeparator(.hidden)
+        .applyModifications { view in
+            if #available(iOS 16.0, *) {
+                view.scrollIndicators(showsVerticalScrollIndicator ? .automatic : .hidden)
+            } else {
+                view
+            }
+        }
         .refreshable {
             await refresh()
         }
@@ -46,14 +53,14 @@ public struct PullableScrollView<Content: View>: View {
             if let tintColor = tintColor {
                 UIRefreshControl.appearance().tintColor = UIColor(tintColor)
             }
-
+            
             UITableView.appearance().showsVerticalScrollIndicator = showsVerticalScrollIndicator
         }
     }
 }
 
 private extension PullableScrollView {
-
+    
     /// Pull to refresh through this async method
     /// in order to achieve a smoother transition on refreshing
     func refresh() async {
@@ -65,4 +72,15 @@ private extension PullableScrollView {
             }
         }
     }
+}
+
+private extension View {
+
+    /// A way to apply conditional modifiers in view
+    /// - Parameter transform: Apply the needed modifiers
+    /// - Returns: The modified view
+    @ViewBuilder func applyModifications<Content: View>(@ViewBuilder transform: (Self) -> Content) -> some View  {
+            transform(self)
+    }
+
 }
